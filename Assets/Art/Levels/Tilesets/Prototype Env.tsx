65d807e95a28ca3b48a38a1d107e118e10952b25
João Magalhães (Jaum) <dragon530698@gmail.com>
2024-12-22 19:24:30 -0300

    Initial

---
+++ Assets/Art/Levels/Tilesets/Prototype Env.tsx
@@ -0,0 +1,98 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Prototype Env" tilewidth="32" tileheight="32" tilecount="14" columns="7">
+ <image source="LB_Prototype_Environment.png" width="224" height="64"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="collider" x="0" y="32">
+    <polygon points="0,0 32,0 32,-16 32,-16 0,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="collider" x="0" y="32">
+    <polygon points="0,0 32,0 32,-32 32,-32 0,-16"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="collider" x="0" y="32">
+    <polygon points="0,0 32,0 32,-32 30,-32 0,-2"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="collider" x="32" y="32">
+    <polygon points="0,0 -32,0 -32,-32 -30,-32 0,-2"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index" id="2">
+   <object id="2" type="collider" x="32" y="32">
+    <polygon points="0,0 -32,0 -32,-32 -32,-32 0,-16"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <objectgroup draworder="index" id="2">
+   <object id="2" type="collider" x="32" y="32">
+    <polygon points="0,0 -32,0 -32,-16 -32,-16 0,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="collider" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="7">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="collider" x="0" y="0">
+    <polygon points="0,0 32,0 32,16 32,16 0,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="collider" x="0" y="0">
+    <polygon points="0,0 32,0 32,32 32,32 0,16"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="9">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="collider" x="0" y="0">
+    <polygon points="0,0 32,0 32,32 30,32 0,2"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="10">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="collider" x="32" y="0">
+    <polygon points="0,0 -32,0 -32,32 -30,32 0,2"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="11">
+  <objectgroup draworder="index" id="2">
+   <object id="2" type="collider" x="32" y="0">
+    <polygon points="0,0 -32,0 -32,32 -32,32 0,16"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="12">
+  <objectgroup draworder="index" id="2">
+   <object id="2" type="collider" x="32" y="0">
+    <polygon points="0,0 -32,0 -32,16 -32,16 0,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="13">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="collider" x="0" y="0" width="32" height="16"/>
+  </objectgroup>
+ </tile>
+</tileset>
